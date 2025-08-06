cf9bf98c81d2c208e7b3b90a3c30c7bd7e20e2b6
lks1996 <mosplee@naver.com>
2025-08-06 11:28:47 +0800

    자동 매수 로직 구성 중....

--- src/main/java/com/example/boot/KisAutoTrade/Service/AutoTradeService.java
+++ src/main/java/com/example/boot/KisAutoTrade/Service/AutoTradeService.java
@@ -1,18 +1,26 @@
 package com.example.boot.KisAutoTrade.Service;
 
 import com.example.boot.KisAutoTrade.DTO.SheetDto;
+import com.example.boot.KisAutoTrade.DTO.StockBalanceResponseDto;
 import com.example.boot.KisAutoTrade.DTO.StockDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class AutoTradeService {
 
-    public DomesticStockService domesticStockService;
-    public SheetDataImportService sheetDataImportService;
+    private final DomesticStockService domesticStockService;
+    private final SheetDataImportService sheetDataImportService;
 
+    public AutoTradeService(DomesticStockService domesticStockService,  SheetDataImportService sheetDataImportService) {
+        this.domesticStockService = domesticStockService;
+        this.sheetDataImportService = sheetDataImportService;
+    }
 
     public void execute() throws Exception {
         // 1. 구글 시트에서 목표 비중 가져오기
@@ -36,9 +44,23 @@ public class AutoTradeService {
         }
 
         // 2. 잔고 조회
-        StockDto stockBalanceDto = new StockDto();
-        domesticStockService.getBalance(stockBalanceDto);
+        StockDto requestDto = new StockDto();
+        String balancerResponse = domesticStockService.getBalance(requestDto);
 
+        ObjectMapper mapper = new ObjectMapper();
+        StockBalanceResponseDto sbrDto = mapper.readValue(balancerResponse, StockBalanceResponseDto.class);
+
+
+        log.info("예수금 총액: {}", sbrDto.getOutput2().get(0).getDncaTotAmt());
+        log.info("현재 보유 종목 수: {}", sbrDto.getOutput1().size());
+
+
+
+//        List<StockBalanceDto> stockBalanceList = response;
+//        ObjectMapper mapper = new ObjectMapper();
+//        List<StockBalanceDto> input1List = mapper.convertValue(
+//                response.get("input1"), new TypeReference<List<StockBalanceDto>>() {}
+//        );
         // 매수가 필요한 종목 리스트 생성.
             // 구글시트 데이터 종목에 내 보유 종목이 있는 경우
                 // 구글시트 데이터 종목의 비율과 내가 보유하고 있는 종목의 비율이 동일하지 않은 경우(소수점은 내림 처리)
