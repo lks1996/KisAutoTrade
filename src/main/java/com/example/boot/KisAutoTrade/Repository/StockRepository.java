2c283040638827b769697ab114b03ac8ff403492
lks1996 <mosplee@naver.com>
2025-09-02 10:38:50 +0800

    미사용 소스 제거.

--- src/main/java/com/example/boot/KisAutoTrade/Repository/StockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.boot.KisAutoTrade.Repository;
-
-import com.example.boot.KisAutoTrade.Entity.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StockRepository extends JpaRepository<Stock, Long> {}
-
