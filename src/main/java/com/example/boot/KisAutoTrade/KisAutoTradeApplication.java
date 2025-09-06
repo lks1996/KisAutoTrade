f7f62a5c9148f40a0fc444b7db3775b326b2a93b
lks1996 <mosplee@naver.com>
2025-09-06 11:38:46 +0800

    application-dev 원복 및 디비 커넥션 오류 수정

--- src/main/java/com/example/boot/KisAutoTrade/KisAutoTradeApplication.java
+++ src/main/java/com/example/boot/KisAutoTrade/KisAutoTradeApplication.java
@@ -1,14 +1,33 @@
 package com.example.boot.KisAutoTrade;
 
+import com.example.boot.KisAutoTrade.Service.AutoTradeService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
-public class KisAutoTradeApplication {
+@SpringBootApplication(
+		exclude = {
+				org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+				org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+		}
+)
+public class KisAutoTradeApplication implements CommandLineRunner {
+
+	private final AutoTradeService autoTradeService;
+
+	public KisAutoTradeApplication(AutoTradeService autoTradeService) {
+		this.autoTradeService = autoTradeService;
+	}
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(KisAutoTradeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		autoTradeService.execute();
+		System.exit(0);
+	}
 }
